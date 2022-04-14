e7a3d7c6048c2873cef98b83ed636b62ecd6c9c8
Milind D <mvdixit@gmail.com>
2022-04-14 00:14:24 -0400

    fixed while loop

--- src/com/ripples/arrays/ArrayDemo.java
+++ src/com/ripples/arrays/ArrayDemo.java
@@ -5,29 +5,35 @@ import java.util.Scanner;
 
 public class ArrayDemo {
 	public static void main(String[] args) {
-
+		boolean until = true;
 		String[] username = { "john", "mike", "tony", "will", "david", "Nick" };
-		int[] userid= {1001,1002,1003,1004,1005,1006};
-		long[] userbalance = {10000,20000,30000,40000,50000,60000,};
-		
-		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter userid:");
-		int uid = scan.nextInt();
-		int match =0;
+		int[] userid = { 1001, 1002, 1003, 1004, 1005, 1006 };
+		long[] userbalance = { 10000, 20000, 30000, 40000, 50000, 60000, };
 
-		//find a userId
-		for (int index = 0; index < userid.length; index++) {
+		Scanner scan = new Scanner(System.in);
+
+		while (until) {
+			System.out.print("Enter userid:");
+			int uid = scan.nextInt();
+			int match = 0;
+
+			// find a userId
+			for (int index = 0; index < userid.length; index++) {
 //			System.out.println("names at index "+index+" : "+userid[index]);
-			
-			//match user given userid with existing userid
-			if (userid[index] == uid) {
-				System.out.println("User is found with userid: "+uid);
-				System.out.println("the Account username: "+username[index]);
-				System.out.println("The Account balance: "+userbalance[index]);
-				match++;
+
+				// match user given userid with existing userid
+				if (userid[index] == uid) {
+					System.out.println("User is found with userid: " + uid);
+					System.out.println("the Account username: " + username[index]);
+					System.out.println("The Account balance: " + userbalance[index]);
+					match++;
+					until = false;
+				}
 			}
-		}if(match == 0) {
-			System.out.println("User is not found with userid "+uid);
+			if (match == 0) {
+				System.out.println("User is not found with userid " + uid);
+				System.out.println("----------------");
+			}
 		}
 	}
 
