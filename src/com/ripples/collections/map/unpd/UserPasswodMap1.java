5c901d01db4302ff7a43af5a1ba772e1033d9c2f
Milind D <mvdixit@gmail.com>
2022-04-17 18:02:19 -0400

    bug in code

--- src/com/ripples/collections/map/unpd/UserPasswodMap1.java
+++ src/com/ripples/collections/map/unpd/UserPasswodMap1.java
@@ -9,23 +9,32 @@ public class UserPasswodMap1 {
 	public static void main(String[] args) {
 
 		Map<String, String> upmap = new LinkedHashMap<String, String>();
+		createNewUser("nw", upmap);
+		
 
+	}
+
+	private static void createNewUser(String a, Map<String, String> b) {
 		boolean until = true;
 		Scanner scan = new Scanner(System.in);
-
+		System.out.println("---------------------------------------------------------------");
+			System.out.println("======:USER REGISTRTION:======");
+			System.out.println("(Please enter username & password to create a new account.)");
+			System.out.println("-----------------------------------------------------------");
+			System.out.println(" ");
 		do {
-			System.out.print("Enter your username:");
+			System.out.print("Enter Username:");
 			String un = scan.nextLine();
-			System.out.print("Enter your password:");
+			System.out.print("Hello "+un+"!... Enter password:");
 			String pw = scan.nextLine();
 
-			String newmap = upmap.put(un, pw);
+			a = b.put(un, pw);
 
-			if (upmap != null) {
-				System.out.println("Username: " + un + " | passowrd: " + pw);
+			if (b != null) {
+//				System.out.println("Username: " + un + " | passowrd: " + pw);
 				System.out.println(" ");
 			}
-			System.out.print("wanna add another entry (y/n)?");
+			System.out.print("wanna add another user (y/n)?");
 			String ans = scan.nextLine();
 
 			if (ans.equals("y") || ans.equals("Y")) {
@@ -35,35 +44,65 @@ public class UserPasswodMap1 {
 			}
 
 		} while (until);
-		
 
 		System.out.println("--------- Database ---------------");
 		System.out.println(" Username: | Password:");
-		for (String id: upmap.keySet()) {
-			System.out.println(" "+id+"         "+upmap.get(id));
-			}
-			System.out.println("----------------------------");
-			System.out.println(" ");
-		
-		System.out.print("::Already Registered? \nEnter your user name:");
-		String uiname = scan.nextLine();
+		for (String id : b.keySet()) {
+			System.out.println(" " + id + "         " + b.get(id));
+		}
+		System.out.println("----------------------------------");
+		System.out.println(" ");
+
+		scan.close();
+		choices("a", b);
+	}
+	
+	public static void choices(String a, Map<String, String> b) {
+		System.out.print("Select Option::(1  2  3) \n1.Registered User? Enter your UserName   \n2.Create New Account  \n3.Exit  \nYour selected option:");
+		Scanner s3 = new Scanner(System.in);
+		int choice = s3.nextInt();
+
+		switch (choice) {
+		case 1:
+			findUser("a", b);
+			break;
+		case 2:
+			createNewUser("nw", b);
+			break;
+		case 3:
+			System.exit(0);
+			break;
+		default:
+			System.out.println("Invalid selection. please choose correct option");
+
+		}
+		s3.close();
+	}
+	
+	public static void findUser(String uiname, Map<String, String> b) {
+		Scanner scan2 = new Scanner(System.in);
+		System.out.print("Enter your registered user name:");
+		uiname = scan2.nextLine();
 //		System.out.println("You entered :" + uiname);
-		if (upmap.containsKey(uiname)) {
-			while (until) {
+		if (b.containsKey(uiname)) {
+			while (true) {
 				System.out.print("Enter your password:");
-				String uipwd = scan.nextLine();
-				if (upmap.get(uiname).equals(uipwd)) {
+				String uipwd = scan2.nextLine();
+				if (b.get(uiname).equals(uipwd)) {
 					System.out.println("Login successful!");
+					System.exit(0);
 					break;
 				} else {
-					System.out.println("Invalid password. please try again!");
+					System.out.println("-----------------------------------------");
+					System.out.println("***Invalid password***. please try again!");
 					continue;
 				}
 			}
 		} else {
-			System.out.println("User not available!");
-			// method:createNewUser();
-		}
+			System.out.println("User not available.");
+			createNewUser("nw", b);
+
+		}scan2.close();
 	}
 
 }
